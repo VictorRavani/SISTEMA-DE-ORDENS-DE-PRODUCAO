6030620589541e205c4de282ea1698c0776f3c07
victorravani98@gmail.com <victorravani98@gmail.com>
2026-03-10 10:57:30 -0300

    update

--- main.py
+++ main.py
@@ -1979,6 +1979,7 @@ def dados():
     
 #app.run(debug=True)
 
-app.run(host='0.0.0.0', port=5000, debug=True)
+#app.run(host='0.0.0.0', port=5000, debug=True)
 
-
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
