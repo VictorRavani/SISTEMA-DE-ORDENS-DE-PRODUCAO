44994aa58bd66a35853932c78d2cc7cd1a1bf57d
victorravani98@gmail.com <victorravani98@gmail.com>
2026-03-10 10:17:41 -0300

    uodate

--- scripts/create_admin.py
+++ scripts/create_admin.py
@@ -1,3 +1,11 @@
+import sys
+import os
+
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
+
+import bcrypt
+from database import postgresDatabase
+
 import bcrypt
 from database import postgresDatabase
 
